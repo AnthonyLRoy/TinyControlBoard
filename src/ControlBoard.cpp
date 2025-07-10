baedd05b6e31bd9f4424b8cc5dbfe384e2a938ea
AnthonyLRoy <Anthonylroy@hotmail.com>
2025-07-10 21:02:50 +0100

    Implement code changes to enhance functionality and improve performance, add test for leds

--- src/ControlBoard.cpp
+++ src/ControlBoard.cpp
@@ -1,6 +1,10 @@
 
 #include "controlboard.hpp"
 #include "powerLed.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
 
 #define PIN_APP_ACTIVE_LED GPIO_NUM_3
 #define PIN_APP_STANDBY_LED GPIO_NUM_4
@@ -10,9 +14,16 @@ namespace controlSystem
 
     void ControlBoard::init()
     {
-        // Example: Initialize power LED
-        indicators::PowerLed powerLed(PIN_APP_ACTIVE_LED, PIN_APP_STANDBY_LED);
-        powerLed.setState(ControlBoardState::Standby);
+        while (1 == 1) // Infinite loop to keep the control board running
+        {
+            // Example: Initialize power LED
+            indicators::PowerLed powerLed(PIN_APP_ACTIVE_LED, PIN_APP_STANDBY_LED);
+            powerLed.setState(ControlBoardState::Standby);
+            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+            powerLed.setState(ControlBoardState::Active);
+
+            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+        }
     }
 
 }
