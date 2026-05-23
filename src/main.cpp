c463c904cee86d276d7cd0e8b5f4ad9ed4600ec4
AnthonyLRoy <Anthonylroy@hotmail.com>
2026-05-23 23:43:23 +0100

    Refactor: Enable power management and configure light sleep settings in main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include "main.h"
 #include "input/input.hpp"
+#include "esp_pm.h"
 
 
 extern "C" void app_main(void)
 {
+    esp_pm_config_t pmConfig = {
+        .max_freq_mhz = 240,
+        .min_freq_mhz = 40,
+        .light_sleep_enable = true
+    };
+    esp_pm_configure(&pmConfig);
+
     vTaskDelay(pdMS_TO_TICKS(5000));  //this is really unfortunate , but we need to wait for the power to stabilise before we start doing anything
 
     controlSystem::ControlBoard board;
